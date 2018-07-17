50fd6098ed04242a38007a2345a90d0bc110ba27
x0a <x0a.cake@gmail.com>
2018-07-17 17:28:07 -0500

    80% performance improvement

--- src/App.js
+++ src/App.js
@@ -1,91 +1,39 @@
 import React, { Component } from 'react';
 import {
   Container, Row, Col,
-  Button, InputGroup, Input, InputGroupAddon,
-  Navbar, NavbarBrand,
-  Card, CardText, CardTitle
+  Button, InputGroup, Input,
+  InputGroupAddon, Navbar, NavbarBrand,
+  Card
 } from "reactstrap";
 import './App.css';
-import memoizeOne from 'memoize-one';
+import RadixConverter from "./RadixConverter.js";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.changeHandler = this.changeHandler.bind(this);
-    this.convert = this.convert.bind(this);
-    this.newHover = this.newHover.bind(this);
+    this.calculate = this.calculate.bind(this);
     this.edit = this.edit.bind(this);
-    this.select = this.select.bind(this);
-    this.selectAll = this.selectAll.bind(this);
 
     this.state = {
       string: "Enter a string here",
       stringArr: [],
-      hover: -1,
-      select: -1,
       radix: 0,
       delimeter: "",
-      regex: false,
-      delimeterDisplay: ""
+      regex: false
     };
   }
 
-  changeHandler(event) {
-    if (event.target.name === "inputStr") {
-      this.setState({ string: event.target.value })
-    } else if (event.target.id === "type") {
-      let radix = ~~event.target.value;
-      let delimeter = radix > 0 ? " " : "";
-      this.setState({ radix: radix, delimeter: delimeter, delimeterDisplay: this.getDelimeterDisplay(delimeter, this.state.regex) });
-    } else if (event.target.name === "delimeter") {
-      let delimeter = event.target.value;
-      this.setState({ delimeter: delimeter, delimeterDisplay: this.getDelimeterDisplay(delimeter, this.state.regex) });
-    }
-  }
 
-  componentDidMount() {
-    this.convert();
-  }
-  getDelimeterDisplay(delimeter, regex) {
-    if (regex) {
-      console.log("/" + JSON.stringify(delimeter) + "/g")
-      return "/" + delimeter + "/g";
-    } else {
-      return delimeter.split("").map(char => "0x" + RadixConverter.convertBase(char.charCodeAt(0), 10, 16)).join(" ")
-    }
-  }
-  convert() {
-    let delimeter = this.state.regex ? new RegExp(this.state.delimeter) : this.state.delimeter;
-
-    this.setState({
-      stringArr: this.state.string.split(delimeter).map(char => {
-        if (this.state.radix === 0)
-          return char.charCodeAt(0)
-        else
-          return RadixConverter.convertBase(char, this.state.radix, 10)
-      })
-    })
-  }
-  select(key) {
-    this.setState({ select: key });
-  }
   edit(radix, digits) {
     this.setState({
       radix: radix,
       string: digits.join(radix === 0 ? "" : " "),
-      regex: false,
-      delimeter: radix > 0 ? " " : ""
     });
   }
 
-  newHover(key) {
-    this.setState({ hover: key });
-  }
-
-  selectAll(event) {
-    event.target.selectionStart = 0;
-    event.target.selectionEnd = event.target.value.length;
+  calculate(string, stringArr) {
+    this.setState({ string: string, stringArr: stringArr })
   }
 
   render() {
@@ -98,53 +46,19 @@ class App extends Component {
 
           <Row>
             <Col>
-              <Card body className="text-right">
-                <Row>
-                  <Col lg="3" md="4" className="mb-1">
-                    <InputGroup>
-                      <InputGroupAddon addonType="prepend">Input Type</InputGroupAddon>
-                      <select className="form-control" id="type" onChange={this.changeHandler} value={this.state.radix}>
-                        <option value="0">Plaintext</option>
-                        <option value="10">Decimal</option>
-                        <option value="16">Hex</option>
-                        <option value="2">Binary</option>
-                        <option value="8">Octal</option>
-                      </select>
-                    </InputGroup>
-                  </Col>
-                  {(this.state.radix > 0 ?
-                    [<Col lg="4" md="5" key="delimeterSet" className="mb-1">
-                      <InputGroup>
-                        <InputGroupAddon addonType="prepend">Delimeter</InputGroupAddon>
-                        <InputGroupAddon addonType="prepend">
-                          <Button color="secondary" outline active={this.state.regex} onClick={() => this.setState({ regex: !this.state.regex, delimeterDisplay: this.getDelimeterDisplay(this.state.delimeter, !this.state.regex) })}>Regex</Button>
-                        </InputGroupAddon>
-                        <Input placeholder="Delimeter" name="delimeter" value={this.state.delimeter} onChange={this.changeHandler} />
-                      </InputGroup>
-                    </Col>,
-                    <Col lg="4" md="3" key="delimeterShow">
-                      {this.state.delimeter.length ? <kbd className="delimeter">{this.state.delimeterDisplay}</kbd> : undefined}
-                    </Col>]
-                    : undefined)
-                  }
-                </Row>
-                <textarea name="inputStr" className="form-control" placeholder="Enter a string here" onChange={this.changeHandler} value={this.state.string} onFocus={this.selectAll} autoFocus="true"></textarea>
-                <div>
-                  <span className="float-left length">Input length: <strong>{this.state.string.length}</strong></span>
-                  <Button className="float-right" color="primary" onClick={this.convert}>Convert</Button>
-                </div>
-              </Card>
+              <StringEditor
+                input={this.state.string}
+                radix={this.state.radix}
+                calculate={this.calculate}
+              />
             </Col>
           </Row>
           <Row>
-            <Col>
-              <RadixConverter radix="0" input={this.state.stringArr} select={this.state.select} onSelect={this.select} hover={this.state.hover} onHover={this.newHover} onEdit={this.edit} />
-              <RadixConverter radix="10" input={this.state.stringArr} select={this.state.select} onSelect={this.select} hover={this.state.hover} onHover={this.newHover} onEdit={this.edit} />
-              <RadixConverter radix="16" input={this.state.stringArr} select={this.state.select} onSelect={this.select} hover={this.state.hover} onHover={this.newHover} onEdit={this.edit} />
-              <RadixConverter radix="8" input={this.state.stringArr} select={this.state.select} onSelect={this.select} hover={this.state.hover} onHover={this.newHover} onEdit={this.edit} />
-              <RadixConverter radix="2" input={this.state.stringArr} select={this.state.select} onSelect={this.select} hover={this.state.hover} onHover={this.newHover} onEdit={this.edit} />
-              <RadixConverter radix="-1" input={this.state.stringArr} select={this.state.select} onSelect={this.select} hover={this.state.hover} onHover={this.newHover} onEdit={this.edit} />
-            </Col>
+            <RadixContainer
+              input={this.state.stringArr}
+              onEdit={this.edit}
+              bases={[0, 10, 16, 8, 2, -1]}
+            />
           </Row>
         </Container>
       </div>
@@ -152,194 +66,173 @@ class App extends Component {
   }
 }
 
-class RadixConverter extends Component {
+class StringEditor extends Component {
   constructor(props) {
     super(props);
+    
+    this.changeHandler = this.changeHandler.bind(this);
+    this.convert = this.convert.bind(this);
 
-    this.edit = this.edit.bind(this);
-
-    if (props.radix) {
-      this.radix = ~~props.radix;
-    } else {
-      this.radix = 10;
-    }
-
-    if (this.radix === -1) {
-      this.radixName = "UTF-8";
-      this.description = "Translation of binary Unicode code points"
-    } else if (this.radix === 0) {
-      this.radixName = "Text"
-    } else if (this.radix === 2) {
-      this.radixName = "Binary";
-    } else if (this.radix === 8) {
-      this.radixName = "Octal";
-    } else if (this.radix === 10) {
-      this.radixName = "Decimal";
-    } else if (this.radix === 16) {
-      this.radixName = "Hexadecimal";
-    }
-
-    this.memoizeInputProp = memoizeOne((input) => this.setState({ input: this.convert(input, this.radix) }));
-    this.state = { input: this.convert(props.input || [], this.radix), hovering: -1, selected: -1 };
+    this.calculate = props.calculate;
+    this.state = {
+      string: props.input || "",
+      radix: props.radix || 0,
+      delimeter: props.radix === 0 ? "" : " ",
+      regex: false
+    };
   }
 
-  componentWillReceiveProps(newProps) {
-    this.memoizeInputProp(newProps.input);
-    this.setState({ hovering: newProps.hover, selected: newProps.select })
-  }
-
-  convert(numbers, to) {
-    return numbers.map(digit => {
-      if (typeof digit === "number" && isNaN(digit))
-        return "NaN";
-      else if (to > 0)
-        return RadixConverter.convertBase(digit, 10, to)
-      else if (to === 0)
-        return String.fromCharCode(digit);
-      else if (to === -1) {
-        let digits = this.dec2utf8(digit);
-
-        if (typeof digits !== "object")
-          return "NaN";
-        else
-          return digits.map(number => number.toString(2).padStart(8, "0")).join(" ");
-      }
+  componentWillReceiveProps(nextProps){
+    this.setDelimeter(nextProps.radix > 0 ? " " : "", this.state.regex);
+    this.setState({
+      string: nextProps.input,
+      radix: nextProps.radix
     })
   }
-  // lot of research and tinkering went into this shit.., for a performance gain of only 60%
-  // but it was still rewarding
-  dec2utf8(number) {
-    if (isNaN(number) || number < 0)
-      return NaN;
-    else if (number < 0x80) // 7 bits or less
-      return [number];
-    else if (number < 0x800) // 11 bits or less
-      return [((number >> 6) & 0x3f) | 0xc0, (number & 0x3f) | 0x80];
-    else if (number < 0x10000) // 16 bits or less
-      return [((number >> 12) & 0x3f) | 0xe0, ((number >> 6) & 0x3f) | 0x80, (number & 0x3f) | 0x80];
-    else if (number < 0x200000) // 21 bits or less
-      return [((number >> 18) & 0x3f) | 0xf0, ((number >> 12) & 0x3f) | 0x80, ((number >> 6) & 0x3f) | 0x80, (number & 0x3f) | 0x80];
-    else
-      return NaN;
+  changeHandler(event) {
+    let state = {};
+
+    if (event.target.name === "inputStr") {
+      state.string = event.target.value;
+    } else if (event.target.id === "type") {
+      state.radix = ~~event.target.value;
+      this.setDelimeter(state.radix > 0 ? " " : "", this.state.regex);
+    } else if (event.target.name === "delimeter") {
+      return this.setDelimeter(event.target.value, this.state.regex);
+    }
+
+    this.setState(state);
   }
 
-  static convertBase(number, from, to) {
-    const codetable = [
-      "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", // Using an array here makes this function
-      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", // nearly 60% faster than a string
-      "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", // at least for binary values.
-      "U", "V", "W", "X", "Y", "Z"
-    ];
+  setDelimeter(delimeter, regex) {
+    let state = {
+      delimeter: delimeter
+    };
 
-    if (isNaN(number) && typeof number === "number") return NaN;
-
-    if (from === 10) {
-      number = parseInt(number, 10);
-
-      if (to === 10) return number;
-      if (codetable.length < to || number < 0) return NaN;
-
-      let digits = [];
-
-      do {
-        let remainder = number % to;
-        digits.push(codetable[remainder]);
-        number = (number - remainder) / to;
-      } while (number !== 0)
-
-      return digits.reverse().join("");
-    } else if (to === 10) {
-      let out = 0;
-
-      if (from > 10) number = number.toUpperCase(); //try not to perform this needlessly
-
-      for (let i = number.length - 1; i !== -1; i--) {
-        let val = codetable.indexOf(number[i]);
-        if (val === -1 || val > from) return NaN;
-        out += val * (from ** (number.length - i - 1));
-      }
-
-      return out;
+    if (regex) {
+      state.delimeterDisplay = "/" + delimeter + "/g";
     } else {
-      return RadixConverter.convertBase(RadixConverter.convertBase(number, from, 10), 10, to);
+      state.delimeterDisplay = delimeter.split("")
+        .map(char => "0x" + RadixConverter.convertBase(char.charCodeAt(0), 10, 16))
+        .join(" ")
     }
+
+    this.setState(state);
   }
 
-
-
-  hover(key) {
-    if (this.props.onHover) {
-      this.props.onHover(key);
-    }
+  componentDidMount() {
+    this.convert();
   }
 
-  unhover(key) {
-    if (key === this.state.hovering)
-      this.hover(-1);
+  convert() {
+    const delimeter = this.state.regex ? new RegExp(this.state.delimeter) : this.state.delimeter;
+
+    this.calculate(this.state.string, this.state.string.split(delimeter).map(char => {
+      if (this.state.radix === 0)
+        return char.charCodeAt(0)
+      else
+        return RadixConverter.convertBase(char, this.state.radix, 10)
+    })
+    )
   }
 
-  select(key, event) {
-    if (this.props.onSelect) {
-      if (key === this.state.selected) {
-        this.props.onSelect(-1);
-      } else {
-        this.props.onSelect(key);
-      }
-    }
-    let selection = getSelection();
-    let range = document.createRange();
-
-    range.selectNodeContents(event.target);
-    selection.removeAllRanges();
-    selection.addRange(range);
-  }
-
-  edit() {
-    if (this.props.onEdit) {
-      let radix = this.radix !== -1 ? this.radix : 2;
-      let input = this.radix !== -1 ? this.state.input : this.state.input.join(" ").split(" ");
-
-      this.props.onEdit(radix, input);
-    }
+  selectAll(event) {
+    event.target.selectionStart = 0;
+    event.target.selectionEnd = event.target.value.length;
   }
 
   render() {
-    return (
-      <Card body className="radix-converter">
-        <CardTitle>
-          <strong>{this.radixName}</strong>
-          {
-            this.description ?
-              <small className="text-muted">{this.description}</small>
-              : undefined
-          }
-          {
-            this.props.onEdit &&
-            <Button size="sm" color="secondary" onClick={this.edit} className="edit-button float-right" href="#">
-              Edit
-            </Button>
-          }
-        </CardTitle>
-        <CardText>{
-          this.state.input.map((digit, key) =>
-            <span
-              className={
-                "digit "
-                + (key === this.state.hovering ? "hover" : "")
-                + " "
-                + (key === this.state.selected ? "select" : "")
-              }
-              onMouseEnter={this.hover.bind(this, key)}
-              onMouseLeave={this.unhover.bind(this, key)}
-              onClick={this.select.bind(this, key)}
-              key={key}
-            >
-              {"" + digit}
-            </span>)
+    return <Card body className="text-right">
+      <Row>
+        <Col lg="3" md="4" className="mb-1">
+          <InputGroup>
+            <InputGroupAddon addonType="prepend">Input Type</InputGroupAddon>
+            <select className="form-control" id="type" onChange={this.changeHandler} value={this.state.radix}>
+              <option value="0">Plaintext</option>
+              <option value="10">Decimal</option>
+              <option value="16">Hex</option>
+              <option value="2">Binary</option>
+              <option value="8">Octal</option>
+            </select>
+          </InputGroup>
+        </Col>
+        {(this.state.radix > 0 ?
+          [<Col lg="4" md="5" key="delimeterSet" className="mb-1">
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">Delimeter</InputGroupAddon>
+              <InputGroupAddon addonType="prepend">
+                <Button color="secondary" outline active={this.state.regex} onClick={
+                  () => {
+                    this.setDelimeter(this.state.delimeter, !this.state.regex);
+                    this.setState({ regex: !this.state.regex });
+                  }
+                }>
+                  Regex
+              </Button>
+              </InputGroupAddon>
+              <Input placeholder="Delimeter" name="delimeter" value={this.state.delimeter} onChange={this.changeHandler} />
+            </InputGroup>
+          </Col>,
+          <Col lg="4" md="3" key="delimeterShow">
+            {this.state.delimeter.length ? <kbd className="delimeter">{this.state.delimeterDisplay}</kbd> : undefined}
+          </Col>]
+          : undefined)
         }
-        </CardText>
-      </Card>
-    )
+      </Row>
+      <textarea name="inputStr" className="form-control" placeholder="Enter a string here" onChange={this.changeHandler} value={this.state.string} onFocus={this.selectAll} autoFocus="true"></textarea>
+      <div>
+        <span className="float-left length">Input length: <strong>{this.state.string.length}</strong></span>
+        <Button className="float-right" color="primary" onClick={this.convert}>Convert</Button>
+      </div>
+    </Card>
+  }
+}
+
+class RadixContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.hover = this.hover.bind(this);
+    this.select = this.select.bind(this);
+    this.addRef = this.addRef.bind(this);
+    this.onEdit = props.onEdit;
+    this.bases = props.bases;
+    this.state = { stringArr: [] };
+    this.instances = [];
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.input) {
+      this.setState({ stringArr: nextProps.input })
+    }
+  }
+
+  select(key) {
+    for (let inst of this.instances) {
+      inst.setSelect(key);
+    }
+  }
+
+  hover(key) {
+    for (let inst of this.instances) {
+      inst.setHover(key);
+    }
+  }
+
+  addRef(inst) {
+    this.instances.push(inst);
+  }
+
+  render() {
+    return <Col>
+      {this.bases.map(radix =>
+        <RadixConverter
+          ref={this.addRef}
+          radix={radix}
+          input={this.state.stringArr}
+          onSelect={this.select}
+          onHover={this.hover}
+          onEdit={this.onEdit} />
+      )}</Col>
   }
 }
 
---
+++ src/RadixConverter.js
@@ -0,0 +1,231 @@
+import React, { Component } from 'react';
+import {
+  Button, Card, CardText, CardTitle
+} from "reactstrap";
+
+import memoizeOne from 'memoize-one';
+
+class RadixConverter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.edit = this.edit.bind(this);
+    this.addRef = this.addRef.bind(this);
+    this.setHover = this.setHover.bind(this);
+
+    if (props.radix !== undefined) {
+      this.radix = ~~props.radix;
+    } else {
+      this.radix = 10;
+    }
+
+    if (this.radix === -1) {
+      this.radixName = "UTF-8";
+      this.description = "Translation of binary Unicode code points"
+    } else if (this.radix === 0) {
+      this.radixName = "Text"
+    } else if (this.radix === 2) {
+      this.radixName = "Binary";
+    } else if (this.radix === 8) {
+      this.radixName = "Octal";
+    } else if (this.radix === 10) {
+      this.radixName = "Decimal";
+    } else if (this.radix === 16) {
+      this.radixName = "Hexadecimal";
+    }
+    this.children = [];
+    this.memoizeInputProp = memoizeOne((input) => {
+      this.prevHover = -1;
+      this.prevSelect = -1;
+      this.setState({ input: this.convert(input, this.radix) })
+    });
+
+    this.state = {
+      input: this.convert(props.input || [], this.radix)
+    };
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.memoizeInputProp(newProps.input);
+  }
+
+  setSelect(select) {
+    if (this.prevSelect !== -1) {
+      this.children[this.prevSelect].classList.remove("select");
+      this.prevSelect = -1;
+    }
+
+    if (select !== -1) {
+      this.prevSelect = select;
+      this.children[select].classList.add("select");
+    }
+  }
+
+  setHover(hover) {
+    if (this.prevHover !== -1) {
+      this.children[this.prevHover].classList.remove("hover");
+      this.prevHover = -1;
+    }
+    
+    if (hover !== -1) {
+      this.prevHover = hover;
+      this.children[hover].classList.add("hover");
+    }
+  }
+
+  convert(numbers, to) {
+    return numbers.map(digit => {
+      if (typeof digit === "number" && isNaN(digit))
+        return "NaN";
+      else if (to > 0)
+        return RadixConverter.convertBase(digit, 10, to)
+      else if (to === 0)
+        return String.fromCharCode(digit);
+      else if (to === -1) {
+        let digits = this.dec2utf8(digit);
+
+        if (digits instanceof Array)
+          return digits.map(number => number.toString(2).padStart(8, "0")).join(" ");
+        else
+          return "NaN";
+      }
+    })
+  }
+  // lot of research and tinkering went into this shit.., for a performance gain of about 60%
+  // but it was still rewarding
+  dec2utf8(number) {
+    if (isNaN(number) || number < 0)
+      return NaN;
+    else if (number < 0x80) // 7 bits or less
+      return [number];
+    else if (number < 0x800) // 11 bits or less
+      return [((number >> 6) & 0x3f) | 0xc0, (number & 0x3f) | 0x80];
+    else if (number < 0x10000) // 16 bits or less
+      return [((number >> 12) & 0x3f) | 0xe0, ((number >> 6) & 0x3f) | 0x80, (number & 0x3f) | 0x80];
+    else if (number < 0x200000) // 21 bits or less
+      return [((number >> 18) & 0x3f) | 0xf0, ((number >> 12) & 0x3f) | 0x80, ((number >> 6) & 0x3f) | 0x80, (number & 0x3f) | 0x80];
+    else
+      return NaN;
+  }
+
+  static convertBase(number, from, to) {
+    const codetable = [
+      "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", // Using an array here makes this function
+      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", // nearly 60% faster than a string
+      "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", // at least for binary values.
+      "U", "V", "W", "X", "Y", "Z"
+    ];
+
+    if (isNaN(number) && typeof number === "number") return NaN;
+
+    if (from === 10) {
+      number = parseInt(number, 10);
+
+      if (to === 10) return number;
+      if (codetable.length < to || number < 0) return NaN;
+
+      let digits = [];
+
+      do {
+        let remainder = number % to;
+        digits.push(codetable[remainder]);
+        number = (number - remainder) / to;
+      } while (number !== 0)
+
+      return digits.reverse().join("");
+    } else if (to === 10) {
+      let out = 0;
+
+      if (from > 10) number = number.toUpperCase(); //try not to perform this needlessly
+
+      for (let i = number.length - 1; i !== -1; i--) {
+        let val = codetable.indexOf(number[i]);
+        if (val === -1 || val > from) return NaN;
+        out += val * (from ** (number.length - i - 1));
+      }
+
+      return out;
+    } else {
+      return RadixConverter.convertBase(RadixConverter.convertBase(number, from, 10), 10, to);
+    }
+  }
+
+  hover(key) {
+    if (this.props.onHover)
+      this.props.onHover(key);
+
+  }
+
+  unhover(key) {
+    if (key === this.prevHover)
+      this.hover(-1);
+  }
+
+  select(key, event) {
+    if (this.props.onSelect) {
+      if (key === this.prevSelect)
+        this.props.onSelect(-1);
+      else
+        this.props.onSelect(key);
+
+    }
+
+    let selection = getSelection();
+    let range = document.createRange();
+
+    range.selectNodeContents(event.target);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+
+  edit() {
+    if (this.props.onEdit) {
+      const UTF8 = this.radix === -1
+      const radix = !UTF8 ? this.radix : 2;
+      const input = !UTF8 ? this.state.input : this.state.input.join(" ").split(" ");
+
+      this.props.onEdit(radix, input);
+    }
+  }
+
+  addRef(key, element) {
+    this.children[key] = element;
+  }
+
+  render() {
+    return (
+      <Card body className="radix-converter">
+        <CardTitle>
+          <strong>{this.radixName}</strong>
+          {
+            this.description ?
+              <small className="text-muted">{this.description}</small>
+              : undefined
+          }
+          {
+            this.props.onEdit &&
+            <Button size="sm" color="secondary" onClick={this.edit} className="edit-button float-right" href="#">
+              Edit
+            </Button>
+          }
+        </CardTitle>
+        <CardText>{
+          this.state.input.map((digit, key) =>
+            <span
+              className="digit"
+              ref={this.addRef.bind(this, key)}
+              onMouseEnter={this.hover.bind(this, key)}
+              onMouseLeave={this.unhover.bind(this, key)}
+              onClick={this.select.bind(this, key)}
+              key={key}
+            >
+              {"" + digit}
+            </span>)
+        }
+        </CardText>
+      </Card>
+    )
+  }
+}
+
+export default RadixConverter;
