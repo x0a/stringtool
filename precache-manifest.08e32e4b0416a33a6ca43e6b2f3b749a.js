cd6492baa2c5c650fdc9164bbf32a40161d31a5e
x0a <x0a.cake@gmail.com>
2019-01-03 21:52:22 -0600

    Updates

---
+++ precache-manifest.08e32e4b0416a33a6ca43e6b2f3b749a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a947f7f10644fe911ee",
+    "url": "/stringtool/static/js/runtime~main.7a947f7f.js"
+  },
+  {
+    "revision": "442841385436cb5dfcbf",
+    "url": "/stringtool/static/js/main.44284138.chunk.js"
+  },
+  {
+    "revision": "b6b5d96a862a183d629f",
+    "url": "/stringtool/static/js/1.b6b5d96a.chunk.js"
+  },
+  {
+    "revision": "442841385436cb5dfcbf",
+    "url": "/stringtool/static/css/main.f01cc691.chunk.css"
+  },
+  {
+    "revision": "e09c2b9778205b7e0f463dc2d9f973e0",
+    "url": "/stringtool/index.html"
+  }
+];
